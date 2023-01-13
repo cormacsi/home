f707042c2a6d55b2087868f3e81eef43af9ae9fe
cormacsi <cormacsi@student.21-school.ru>
2023-01-14 01:59:04 +0300

    typed

---
+++ js/typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+  strings: ["Coding...", "Sleeping...", "Eating...", "Studyng...", "Looking forward to your reply"],
+  typeSpeed: 200,
+  backSpeed: 200,
+  loop: true
+})
